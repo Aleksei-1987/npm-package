179b1155bf259294f342e4218e6d299803222e15
Aleksei Barkasov <alekseibarkasov@gmail.com>
2025-06-06 20:52:52 +0300

    corrected

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,18 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   pluginReact.configs.flat.recommended,
+  globalIgnores(["dist/*"]),
 ]);
-
-
-  ignorePatterns: [
-    'dist/*'
-  ]
